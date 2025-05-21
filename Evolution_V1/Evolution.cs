a796d166b12121e8a9f8a394cff69d8c2b4c7daf
IkayImonoko <ntolok1990@gmail.com>
2025-05-21 15:41:22 +0200

    add habitat class

--- Evolution_V1/Evolution.cs
+++ Evolution_V1/Evolution.cs
@@ -1,10 +1,28 @@
+using System.Drawing;
+
 namespace Evolution_V1;
 
 internal class Evolution
 {
     private Bear _bear = new Bear();
+    private Habitat[] _habitats = new Habitat[4]
+    {
+        new Habitat("Ice", ConsoleColor.White, new Point(0,0)),
+        new Habitat("Forest", ConsoleColor.Green, new Point(50,0)),
+        new Habitat("Desert", ConsoleColor.Yellow, new Point(0,50)),
+        new Habitat("Sea", ConsoleColor.Blue, new Point(50,50))
+    };
     public void Run()
     {
-        throw new NotImplementedException();
+        //Console.SetWindowSize(500, 500);
+        //Console.SetBufferSize(500, 500);
+        Console.Clear();
+        foreach (Habitat habitat in _habitats)
+        {
+            habitat.Draw();
+        }
+
+        Console.ReadKey();
+        
     }
 }
---
+++ Evolution_V1/Habitat.cs
@@ -0,0 +1,33 @@
+﻿using System.Drawing;
+
+namespace Evolution_V1;
+
+public class Habitat
+{
+    private int _length = 50;
+    public Point TopLeft;
+    public string Name;
+    public ConsoleColor Color;
+
+    public Habitat(string name, ConsoleColor color, Point topLeft)
+    {
+        Name = name;
+        Color = color;
+        TopLeft = topLeft;
+    }
+
+    public void Draw()
+    {
+        for (int i = TopLeft.X; i < TopLeft.X + _length; i++)
+        {
+            for (int j = TopLeft.Y; j < TopLeft.Y + _length; j++)
+            {
+                Console.SetCursorPosition(i,j);
+                Console.BackgroundColor = Color;
+                Console.Write("1");
+            }
+        }
+
+        Console.BackgroundColor = ConsoleColor.Black;
+    }
+}
