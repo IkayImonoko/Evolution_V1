a98907b6fd0ee6f98764ad07e0392b274b8639c5
IkayImonoko <ntolok1990@gmail.com>
2025-05-22 15:33:16 +0200

    oops, forgot this file

--- Evolution_V1/Bear.cs
+++ Evolution_V1/Bear.cs
@@ -1,17 +1,35 @@
+using OpenCvSharp;
+
+
 namespace Evolution_V1;
 
 internal class Bear
 {
-    private float _color;
-    private int _lifetime = 365 * 20;//days
+    Scalar _color;
+    public int Lifetime = 365 * 20;//days
+    public Point Coordinates;
 
-    public Bear(float color=0)
-    {
-        _color = color;
+    public Bear()
+    { 
+        Random random = new Random();
+        _color = new Scalar(random.Next(0, 255), random.Next(0, 255), random.Next(0, 255));
+        Coordinates.X = random.Next(0, 800);//TODO Magic number - size of Main field
+        Coordinates.Y = random.Next(0, 800);//
+    }
+    
+    public Bear(Scalar colorA, Scalar colorB)
+    { 
+        _color = new Scalar((colorA.Val0 + colorB.Val0)/(double)2, (colorA.Val1 + colorB.Val1)/(double)2, (colorA.Val2 + colorB.Val2)/(double)2);
     }
 
-    public static Bear MakeChild(float colorA, float color)
+    public static Bear MakeChild(Scalar colorA, Scalar colorB)
     {
-        
+        return new Bear(colorA, colorB);
+    }
+
+    public void Draw(Mat mainField)
+    {
+        mainField.Circle(Coordinates.X, Coordinates.Y, 3, _color, -1);
+        mainField.Circle(Coordinates.X, Coordinates.Y, 3, Scalar.Black, 1);
     }
 }
