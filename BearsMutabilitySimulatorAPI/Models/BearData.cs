9b3375d548548e1b17ca678e2bd1db7790953cae
IkayImonoko <ntolok1990@gmail.com>
2025-06-06 15:16:13 +0200

    Started API

---
+++ BearsMutabilitySimulatorAPI/Models/BearData.cs
@@ -0,0 +1,10 @@
+﻿namespace BearsMutabilitySimulatorAPI.Models;
+
+public class BearData
+{
+    public double[] Color { get; set;}
+    public double[] MaximumAllowablePosition { get; set;}
+    public int Lifetime { get; set; }
+    public double[] Position { get; set; }
+    public int TimeToNextChildLeft { get; set; }
+}
---
+++ BearsMutabilitySimulatorAPI/Program.cs
@@ -0,0 +1,39 @@
+using BearsMutabilitySimulatorAPI.Models;
+using BearsMutabilitySimulatorLib;
+using OpenCvSharp;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+int bearsAmount = 50;
+int habitatLenght = 400;
+var coordinateSpace = new CoordinateSpace(new Point(0,0), habitatLenght * 2, habitatLenght * 2);
+
+List<Bear> bears = new List<Bear>(bearsAmount);
+bears.AddRange(Enumerable.Range(0, bearsAmount).Select(_ => new Bear(coordinateSpace.BottomRight)));
+
+app.MapGet("/bears", () =>
+{
+    var bearsData = bears.Select(b => new BearData()
+    {
+        Color = [b.Color.Val0, b.Color.Val1, b.Color.Val2],
+        MaximumAllowablePosition = [b.MaximumAllowablePosition.X, b.MaximumAllowablePosition.Y],
+        Lifetime = b.Lifetime,
+        Position = [b.Position.X, b.Position.Y],
+        TimeToNextChildLeft = b.TimeToNextChildLeft
+    }).ToArray();
+    return bearsData;
+});
+    
+
+app.Run();
