5bbc74773a46eccf210767076a96beb4fbb87167
IkayImonoko <ntolok1990@gmail.com>
2025-07-10 22:38:53 +0200

    current uppdate

--- BearsMutabilitySimulatorLib/Bear.cs
+++ BearsMutabilitySimulatorLib/Bear.cs
@@ -17,14 +17,8 @@ public class Bear
         TimeToNextChildLeft = 730;
         Lifetime = random.Next(365 * 5, 365 * 10);
         var mutation = random.Next(1, 11);
-        if (mutation == 1)
-        {
-            Color = new Scalar(random.Next(0, 256), random.Next(0, 256), random.Next(0, 256));
-        }
-        else
-        {
-            Color = new Scalar((colorA.Val0 + colorB.Val0) / 2, (colorA.Val1 + colorB.Val1) / 2, (colorA.Val2 + colorB.Val2) / 2);
-        }
+        Color = mutation == 1 ? new Scalar(random.Next(0, 256), random.Next(0, 256), random.Next(0, 256)) : 
+            new Scalar((colorA.Val0 + colorB.Val0) / 2, (colorA.Val1 + colorB.Val1) / 2, (colorA.Val2 + colorB.Val2) / 2);
 
         Position = new Point(random.Next(0, maximumAllowablePosition.X), random.Next(0, maximumAllowablePosition.Y));
         
