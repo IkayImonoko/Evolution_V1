d98fb1449155bd96934dfb328d5be5e172b1910b
IkayImonoko <ntolok1990@gmail.com>
2025-07-16 23:09:34 +0200

    current uppdate

--- BearsMutabilitySimulatorLib/BearsController.cs
+++ BearsMutabilitySimulatorLib/BearsController.cs
@@ -2,20 +2,11 @@
 
 namespace BearsMutabilitySimulatorLib;
 
-public class BearsController
+public class BearsController(CoordinateSpace coordinateSpace, List<Bear> bears)
 {
-    private CoordinateSpace _coordinateSpace;
-    private List<Bear> _bears;
-    
-    public BearsController(CoordinateSpace coordinateSpace, List<Bear> bears)
-    {
-        _bears = bears;
-        _coordinateSpace = coordinateSpace;
-    }
-    
     private Bear? MakeChild(Bear parrentA, Bear parrentB)
     {
-        var newBear = new Bear(_coordinateSpace.BottomRight, parrentA.Color, parrentB.Color);
+        var newBear = new Bear(coordinateSpace.BottomRight, parrentA.Color, parrentB.Color);
         var random = new Random();
         newBear.Position = new Point(
             parrentA.Position.X + random.Next(-20, 21),
@@ -24,7 +15,7 @@ public class BearsController
         parrentA.TimeToNextChildLeft = 730;
         parrentB.TimeToNextChildLeft = 730;
         
-        return _coordinateSpace.CanMoveTo(newBear.Position) ? newBear : null;
+        return coordinateSpace.CanMoveTo(newBear.Position) ? newBear : null;
     }
     
     private void Move(Bear bear)
@@ -42,7 +33,7 @@ public class BearsController
         var step = new Step(bear.Position);
         var newPosition = step.MakeStep((Direction)stepDirection, stepLength);
 
-        while (!_coordinateSpace.CanMoveTo(newPosition))
+        while (!coordinateSpace.CanMoveTo(newPosition))
         {
             stepDirection = random.Next(0, 9);
             newPosition = step.MakeStep((Direction)stepDirection, stepLength);
@@ -53,12 +44,12 @@ public class BearsController
     
     private void CheckCollisionsAndSpawnBears()
     {
-        for (var i = 0; i < _bears.Count - 1; i++)
+        for (var i = 0; i < bears.Count - 1; i++)
         {
-            for (var j = i + 1; j < _bears.Count; j++)
+            for (var j = i + 1; j < bears.Count; j++)
             {
-                var bear1 = _bears[i];
-                var bear2 = _bears[j];
+                var bear1 = bears[i];
+                var bear2 = bears[j];
                 var distance = GetDistanceBetweenBears(bear1, bear2);
                 if (!(distance < 6) ||
                     bear1.TimeToNextChildLeft != 0 ||
@@ -66,7 +57,7 @@ public class BearsController
                 var newBear = MakeChild(bear1, bear2);
                 if (newBear != null)
                 {
-                    _bears.Add(newBear); 
+                    bears.Add(newBear); 
                 }
                 break;
             }
@@ -83,8 +74,8 @@ public class BearsController
 
     public void RunOneIteration()
     {
-        _bears.RemoveAll(b => b.Lifetime == 0);
-        _bears.ForEach(Move);
+        bears.RemoveAll(b => b.Lifetime == 0);
+        bears.ForEach(Move);
         CheckCollisionsAndSpawnBears();
     }
     
