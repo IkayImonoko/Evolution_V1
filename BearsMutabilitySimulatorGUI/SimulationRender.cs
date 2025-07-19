b5f156924b539e9a728d7890418ccd2ce7f1f64f
IkayImonoko <ntolok1990@gmail.com>
2025-07-19 21:00:29 +0200

    current uppdate

--- BearsMutabilitySimulatorGUI/SimulationRender.cs
+++ BearsMutabilitySimulatorGUI/SimulationRender.cs
@@ -4,27 +4,18 @@ using OpenCvSharp;
 
 namespace BearsMutabilitySimulatorGUI;
 
-public class SimulationRender
+public class SimulationRender(CoordinateSpace coordinateSpace, List<Bear> bears, Habitat[] habitats)
 {
-    private readonly Mat _mainField;
-    private readonly List<Bear> _bears;
-    private readonly Habitat[] _habitats;
+    private readonly Mat _mainField = new(coordinateSpace.Height, coordinateSpace.Width, MatType.CV_8UC3, Scalar.Red);
 
-    public SimulationRender(CoordinateSpace coordinateSpace, List<Bear> bears, Habitat[] habitats)
-    {
-        _bears = bears;
-        _habitats = habitats;
-        _mainField = new Mat(coordinateSpace.Height, coordinateSpace.Width, MatType.CV_8UC3, Scalar.Red);
-
-    }
     public void Render()
     {
-        foreach (var habitat in _habitats)
+        foreach (var habitat in habitats)
         {
             var habitatPosition = new Rect(habitat.TopLeft.X, habitat.TopLeft.Y, habitat.Length, habitat.Length);
             _mainField[habitatPosition].SetTo(habitat.Color);
         }
-        _bears.ForEach(b =>
+        bears.ForEach(b =>
         {
             _mainField.Circle(b.Position.X, b.Position.Y, 3, b.Color, -1);
             _mainField.Circle(b.Position.X, b.Position.Y, 3, Scalar.Black, 1);
